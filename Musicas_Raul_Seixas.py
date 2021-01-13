0998837e5ca92a860384e619c73df7245754a0fd
Erickson-lopes-dev <ofc.erickson@gmail.com>
2021-01-13 10:05:51 -0300

    finalizando

--- Musicas_Raul_Seixas.py
+++ Musicas_Raul_Seixas.py
@@ -1,56 +1,56 @@
 import requests
 from bs4 import BeautifulSoup
+from prettytable import PrettyTable
 
+palavra = 'diabo'
+
+# Página principal com todas as musicas dos artistas
 html = requests.get('https://www.vagalume.com.br/raul-seixas/')
-#
-# soup = BeautifulSoup(html.content, 'html.parser')
-#
-# links = []
-#
-# for tag in soup.find(id='alfabetMusicList').find_all('a'):
-#     caminhno = tag.attrs['href']
-#     if '#play' not in str(caminhno):
-#         links.append('https://www.vagalume.com.br' + caminhno)
-#
-# print(links)
-#
-# html = requests.get('https://www.vagalume.com.br/raul-seixas/o-dia-em-que-a-terra-parou.html')
-#
-# soup = BeautifulSoup(html.content, 'html.parser')
-#
-# # print(soup.find(id="lyrics").contents)
-#
-# palavra = 'não tava lá'
-#
-# for linhas in soup.find(id="lyrics").contents:
-#     if palavra in str(linhas):
-#         print(linhas)
-#
-#     # if str(linhas) != '<br/>':
-#     #     print(linhas)
 
-
+# Estruturando dados como html
 soup = BeautifulSoup(html.content, 'html.parser')
 
 links = []
 
+# Procura a lista alfabetica e filtra todos as tags a
 for tag in soup.find(id='alfabetMusicList').find_all('a'):
+    # Por cada tag pega o conteúdo do atributo href
     caminhno = tag.attrs['href']
+    # filtra os links para que não sej adicionado os links com #play no final
     if '#play' not in str(caminhno):
+        # Gera o link para acessar a música e adiciona na lista
         links.append('https://www.vagalume.com.br' + caminhno)
+        # # printa para exibir o caminho completo
+        # print('https://www.vagalume.com.br' + caminhno)
 
-for link_musica in links:
+frases_encontradas = []
+for num, link_musica in enumerate(links):
     html_musica = requests.get(link_musica)
     soup = BeautifulSoup(html_musica.content, 'html.parser')
 
+    # # printa um objeto bs4 com toda a letra da musica
     # print(soup.find(id="lyrics").contents)
-
-    palavra = 'jesus'
 
     try:
         for linhas in soup.find(id="lyrics").contents:
-            if palavra in str(linhas):
-                print(linhas)
+            if palavra.lower() in str(linhas).lower():
+                frases_encontradas.append(
+                    dict(musica=soup.find(id="lyricContent").find('h1').string, frase=str(linhas).lower())
+                )
+                print(soup.find(id='lyricContent').find('h1').string, ':')
+                print(f'{" " * 3 + linhas}. \n')
     except AttributeError as err:
         pass
+
+print('\n'*3)
+
+# tabela_final = PrettyTable(['Música', 'Frase a Palavra encontrada'])
+
+[print(i) for n, i in enumerate(frases_encontradas) if i not in frases_encontradas[n + 1:]]
+
+# for item in frases_encontradas:
+#     tabela_final.add_row([str(item[0]), str(item[1])])
+
+# print(tabela_final)
+
 
---
+++ teste.py
@@ -0,0 +1,19 @@
+from prettytable import PrettyTable
+
+frase = [
+    {'musica': 'Anarkilópolis', 'frase': 'pra o diabo, os que me chamaram aqui'},
+    {'musica': 'Capim Guiné', 'frase': 'eu tô virado do diabo, eu tô retado cum você'},
+    {'musica': 'Conversa Pra Boi Dormir', 'frase': 'que o diabo fez, enquanto deus marcou'},
+    {'musica': 'D.D.I. (discagem Direta Inter-estelar)', 'frase': 'que o diabo diz que vai baixar de uma vez por aí'},
+    {'musica': 'D.D.I. (discagem Direta Inter-estelar)', 'frase': 'pode ser um trote do diabo'},
+    {'musica': 'Eu Sou Eu, Nicuri é O Diabo', 'frase': 'eu sou eu, nicuri é o diabo,'},
+    {'musica': 'Eu Sou Eu, Nicuri é O Diabo', 'frase': 'eu sou eu, nicuri é o diabo.'},
+]
+
+print(frase)
+
+tabela_final = PrettyTable(['Música', 'Frase a Palavra encontrada'])
+
+[tabela_final.add_row([i['musica'], i['frase']]) for n, i in enumerate(frase) if i not in frase[n + 1:]]
+
+print(tabela_final)
